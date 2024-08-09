235c8571e02b8bcf2385c6757a4863a8408e44b9
Carson Bring <carsonbring@gmail.com>
2024-08-09 09:32:02 -0500

    Added treesitter and other web dev stuff

--- init.el
+++ init.el
@@ -23,13 +23,16 @@
 (setq package-archives
 '(("GNU ELPA" . "https://elpa.gnu.org/packages/")
 ("MELPA Stable" . "https://stable.melpa.org/packages/")
-("MELPA" . "https://melpa.org/packages/"))
+("MELPA" . "https://melpa.org/packages/")
+("GNU DEVEL" . "https://elpa.gnu.org/devel/"))
 package-archive-priorities
 '(("MELPA Stable" . 10)
-("MELPA" . 5)
+  ("MELPA" . 5)
+  ("GNU DEVEL" . 3)
 ("GNU ELPA" . 2)))
 (package-initialize)
 
+ (setq package-install-upgrade-built-in t)
 ;; custom startup screen into sicp due to me doing my practice
 (setq inhibit-startup-screen t)
 (setq initial-buffer-choice "~/projects")
@@ -64,7 +67,11 @@ package-archive-priorities
                
 ;; Provides all the racket support
 (use-package racket-mode
-             :ensure t)
+  :ensure t)
+
+;; Provides multiple cursors
+(use-package multiple-cursors
+  :ensure t)
 
 ;; Syntax checking
 (use-package flycheck
@@ -106,6 +113,10 @@ package-archive-priorities
 (use-package uniquify
              :config (setq uniquify-buffer-name-style 'post-forward))
 
+; Magit
+(use-package magit
+  :ensure t)
+
 ;; Highlight matching parenthesis
 (show-paren-mode 1)
 (setq show-paren-delay 0)
@@ -136,6 +147,63 @@ package-archive-priorities
   (setq pyvenv-workon ".venv")
   (pyvenv-mode 1))
 
+;;Apheleia setup (prettier)
+(use-package apheleia
+  :ensure t)
+
+;; envrc (determines project env varibales and sets those vars on per-buffer basis)
+;; Create .envrc files to have processes in buffer launch with those env vars
+(use-package envrc
+  :ensure t)
+
+;; Add node modules to path
+;; Usage: M-x add-node-modules-path
+(use-package add-node-modules-path
+  :ensure t)
+
+;;Treesitter
+(use-package tree-sitter
+  :ensure t)
+(use-package tree-sitter-langs
+  :ensure t)
+
+;;Treesit-auto for ts grammar
+(use-package treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
+(setq treesit-auto-install 'prompt)
+
+
+;;tide setup
+(use-package tide
+  :ensure t)
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+;; if you use treesitter based typescript-ts-mode (emacs 29+)
+(add-hook 'typescript-ts-mode-hook #'setup-tide-mode)
+(add-hook 'tsx-ts-mode-hook #'setup-tide-mode)
+;;org-mode setup
+(setq org-startup-indented t)
+(setq org-hide-leading-stars t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -143,7 +211,7 @@ package-archive-priorities
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(spinner lsp-mode rainbow-delimiters paredit company flycheck racket-mode smex magit geiser-racket geiser-mit)))
+   '(treesit-auto spinner lsp-mode rainbow-delimiters paredit company flycheck racket-mode smex magit geiser-racket geiser-mit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
